c29fa5275b5bada24185c243b7820878bbc04691
raghav <raghav03ef@gmail.com>
2024-11-27 12:13:59 +0530

    Add appRouter in App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import SideSection from './components/SideSection'
-import MainSection from './components/MainSection'
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    RouterProvider,
+    Route,
+} from 'react-router-dom'
+
+import Home from './components/Home'
+import TrendingTab from './components/TrendingTab'
+import InboxTab from './components/InboxTab'
+
+const appRouter = createBrowserRouter(
+    createRoutesFromElements(
+        <Route
+            path="/"
+            element={<Home />}
+        >
+            <Route
+                path="/trending"
+                element={<TrendingTab />}
+            />
+            <Route
+                path="/inbox"
+                element={<InboxTab />}
+            />
+        </Route>
+    )
+)
 
 function App() {
-    return (
-        <div className="App">
-            <SideSection />
-            <MainSection />
-        </div>
-    )
+    return <RouterProvider router={appRouter} />
 }
 
 export default App
