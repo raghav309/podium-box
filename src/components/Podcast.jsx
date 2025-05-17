360411d1d3059debfccabda698f4c1b975a57f6c
raghav309 <raghav03ef@gmail.com>
2025-05-17 20:23:36 +0530

    Migrate styles from scss to tailwind

--- src/components/Podcast.jsx
+++ src/components/Podcast.jsx
@@ -1,24 +1,28 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 function Podcast({ podcastDetail }) {
+    const styleListItem =
+        'Podcast rrd-link p-4 flex flex-row items-center gap-4 bg-white border border-[#c4c4c4] rounded-md hover:bg-[#c7c7c7] hover:cursor-pointer';
+
     return (
         <Link
-            className="rrd-link Podcast"
+            className={styleListItem}
             to=""
         >
-            <div className="Podcast-thumbnail">
+            <div className="h-16 w-16">
                 <img
+                    className="w-full"
                     src={podcastDetail.image}
                     alt=""
                 />
             </div>
 
-            <div className="Podcast-detail">
-                <p className="Podcast-title">{podcastDetail.title}</p>
-                <p className="Podcast-author">{podcastDetail.author}</p>
+            <div>
+                <p className="text-xl font-medium">{podcastDetail.title}</p>
+                <p className="text-sm">{podcastDetail.author}</p>
             </div>
         </Link>
-    )
+    );
 }
 
-export default Podcast
+export default Podcast;
