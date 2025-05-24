5fb0a1142395a91a5bba9e9259d56ef83bff0cff
raghav309 <raghav03ef@gmail.com>
2025-05-24 14:27:10 +0530

    Create apiSlice and use it

--- src/state/index.js
+++ src/state/index.js
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import uiReducers from './uiSlice';
+import apiSlice from './apiSlice';
 
 const store = configureStore({
     reducer: {
         ui: uiReducers,
+        api: apiSlice.reducer,
     },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({}).concat([apiSlice.middleware]),
 });
 
 export default store;
