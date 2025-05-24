ea122dc518d0985eaaba619dd75cfc7b324278cd
raghav309 <raghav03ef@gmail.com>
2025-05-24 13:46:37 +0530

    Add state and fix typos

--- src/components/NavList.jsx
+++ src/components/NavList.jsx
@@ -4,22 +4,23 @@ import SubscriptionsOutlinedIcon from '@mui/icons-material/SubscriptionsOutlined
 import WhatshotOutlinedIcon from '@mui/icons-material/WhatshotOutlined';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import NavListItem from './NavListItem';
-
 /*
     this is all the options avilabe in icons 
         <PlaylistPlayOutlinedIcon sx={{ fontSize: 40 }} />
 */
 
 function NavList() {
+    const tab = ['trending', 'inbox', 'subscriptions', 'playlists', 'account'];
+
     return (
         <ul className="NavList p-2 flex flex-row md:flex-col justify-around items-center xl:items-start list-none">
             <NavListItem label="Trending" url="/trending">
                 <WhatshotOutlinedIcon sx={{ fontSize: 32 }} />
             </NavListItem>
-            <NavListItem label="Indbox" url="/inbox">
+            <NavListItem label="Inbox" url="/inbox">
                 <FileDownloadOutlinedIcon sx={{ fontSize: 32 }} />
             </NavListItem>
-            <NavListItem label="Subscritptions" url="/subscription">
+            <NavListItem label="Subscritptions" url="/subscriptions">
                 <SubscriptionsOutlinedIcon sx={{ fontSize: 32 }} />
             </NavListItem>
             <NavListItem label="Playlists" url="/playlists">
--- src/components/NavListItem.jsx
+++ src/components/NavListItem.jsx
@@ -1,11 +1,22 @@
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { activateTab, getActiveTab } from '../state/uiSlice';
 
-function NavListItem({ label, url, children }) {
+function NavListItem({ label, url, onClick, children }) {
+    const styleActive = useSelector(getActiveTab) === label ? 'bg-gray-400' : '';
+    const dispatch = useDispatch();
+    const onClickListItem = (event) => {
+        event.stopPropagation();
+        dispatch(activateTab(label));
+    };
+
     return (
         <li className="xl:w-full">
             <Link
-                className="NavList-item p-2 flex items-center gap-4 no-underline text-black rounded-md hover:bg-[#c7c7c7] hover:cursor-pointer"
+                className={`NavList-item p-2 flex items-center gap-4 no-underline text-black rounded-md hover:bg-gray-300 hover:cursor-pointer ${styleActive}`}
                 to={url}
+                onClick={onClickListItem}
             >
                 {children}
                 <p className="hidden xl:block">{label}</p>
