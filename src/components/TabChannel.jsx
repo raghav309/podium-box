b2e05eca9db7cf32882b50efdceb2be81d807ede
raghav309 <raghav03ef@gmail.com>
2025-05-26 09:40:48 +0530

    Use hooks to load data from backend for list

--- src/components/TabChannel.jsx
+++ src/components/TabChannel.jsx
@@ -1,25 +1,54 @@
 import { useParams } from 'react-router-dom';
 import TabTitle from './TabTitle';
+import List from './List';
+import ListItem from './ListItem';
+import ListItemControls from './ListItemControls';
+import Button from './Button';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import { useGetChannelByFeedIdQuery } from '../state/apiSlice';
+import trendingEpisodes from '../data/trendingData';
 
-function TabChannel({ channelName }) {
+function TabChannel() {
     const { channelId } = useParams();
     const { data, isLoading, isSuccess, isError, error } = useGetChannelByFeedIdQuery(channelId);
-
     let content;
+
     if (isLoading) {
-        content = 'feed is loading...';
+        content = <TabTitle title="feed is loading..." />;
     } else if (isSuccess) {
-        content = data.feed.title;
+        content = (
+            <>
+                <TabTitle title="Channel" />
+                <section id="TabBody">
+                    <section id="ChannelHero" className="py-4 bg-white">
+                        <div className="h-16 w-16">
+                            <img className="w-full rounded-md" src={data.feed.image} alt="" />
+                        </div>
+                        <div id="ChannelDetails">
+                            <p>{data.feed.title}</p>
+                            <p>{data.feed.author}</p>
+                        </div>
+                        <p>{data.feed.description}</p>
+                    </section>
+                    <List>
+                        {trendingEpisodes.map((podcastDetail) => (
+                            <ListItem podcastDetail={podcastDetail}>
+                                <ListItemControls>
+                                    <Button>
+                                        <PlayArrowIcon sx={{ fontSize: 32 }} />
+                                    </Button>
+                                </ListItemControls>
+                            </ListItem>
+                        ))}
+                    </List>
+                </section>
+            </>
+        );
     } else if (isError) {
         console.log(error);
     }
 
-    return (
-        <>
-            <TabTitle title={content} />
-        </>
-    );
+    return <>{content}</>;
 }
 
 export default TabChannel;
--- src/components/TrendingTab.jsx
+++ src/components/TrendingTab.jsx
@@ -1,12 +1,26 @@
-import PodcastList from './PodcastList';
+import List from './List';
+import ListItem from './ListItem';
 import TabTitle from './TabTitle';
-import trendingEpisodes from '../data/trendingData';
+import ListItemLoader from './ListItemSkeleton';
+import { useGetTrendingQuery } from '../state/apiSlice';
 
 function TrendingTab() {
+    const { data, isLoading, isSuccess, isError, error } = useGetTrendingQuery();
+
+    let content;
+    if (isLoading) {
+        content = [0, 1, 2, 3, 4, 5].map(() => <ListItemLoader />);
+    } else if (isSuccess) {
+        console.log(data.feeds);
+        content = data.feeds.map((podcastDetail) => <ListItem podcastDetail={podcastDetail} />);
+    } else if (isError) {
+        content = <p>oops feed cannot be loaded, there is an error</p>;
+    }
+
     return (
         <>
             <TabTitle title="Trending podcasts:" />
-            <PodcastList dataArr={trendingEpisodes} />
+            <List>{content}</List>
         </>
     );
 }
