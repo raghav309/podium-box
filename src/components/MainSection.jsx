554010985f6a5a5f9f945e513b67c84188c466b1
raghav309 <raghav03ef@gmail.com>
2025-05-26 09:45:57 +0530

    Fix minor styles issues

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -3,12 +3,12 @@ import AudioPlayer from './AudioPlayer';
 
 function MainSection() {
     return (
-        <section className="h-full w-auto relative order-1 overflow-hidden md:h-auto md:w-full bg-[#f1f1f1]">
-            <div className="h-full w-fit mx-auto my-0 relative overflow-y-auto">
+        <main className="h-full w-auto relative order-1 overflow-hidden md:h-auto md:w-full">
+            <div id="tab" className="h-full max-w-2xl mx-auto my-0 px-4 relative overflow-y-auto">
                 <Outlet />
             </div>
             <AudioPlayer />
-        </section>
+        </main>
     );
 }
 
--- src/components/TabTitle.jsx
+++ src/components/TabTitle.jsx
@@ -1,5 +1,9 @@
 function TabTitle({ title }) {
-    return <h1 className="mx-0 my-4 sticky top-0 bg-white">{title}</h1>;
+    return (
+        <h1 className="mx-0 mb-4 py-4 sticky top-0 text-xl font-semibold leading-none bg-white border-b border-b-[#c4c4c4]">
+            {title}
+        </h1>
+    );
 }
 
 export default TabTitle;
