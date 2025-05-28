b48ab64389dc41548e257fa36435c69e6ada116a
raghav309 <raghav03ef@gmail.com>
2025-05-28 23:23:22 +0530

    Create reducer and action for player

--- src/state/uiSlice.js
+++ src/state/uiSlice.js
@@ -2,7 +2,14 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const uiSlice = createSlice({
     name: 'ui',
-    initialState: { activeTab: 'Trending', isAudioPlaying: false },
+    initialState: {
+        activeTab: 'Trending',
+        isAudioPlaying: false,
+        audioDuration: 0,
+        audioProgress: 0,
+        audioRef: null,
+        progressBarRef: null,
+    },
     reducers: {
         activateTab: (state, action) => {
             state.activeTab = action.payload;
@@ -10,10 +17,33 @@ const uiSlice = createSlice({
         toggleAudio: (state) => {
             state.isAudioPlaying = !state.isAudioPlaying;
         },
+        audioDuration: (state, action) => {
+            state.audioDuration = action.payload;
+        },
+        audioProgress: (state, action) => {
+            state.audioProgress = action.payload;
+        },
+        setAudioRef: (state, action) => {
+            state.audioRef = action.payload;
+        },
+        setProgressBarRef: (state, action) => {
+            state.progressBarRef = action.payload;
+        },
     },
 });
 
-export const { activateTab, toggleAudio } = uiSlice.actions;
+export const {
+    activateTab,
+    toggleAudio,
+    audioDuration,
+    audioProgress,
+    setAudioRef,
+    setProgressBarRef,
+} = uiSlice.actions;
 export const getActiveTab = (state) => state.ui.activeTab;
 export const getAudioPlayerState = (state) => state.ui.isAudioPlaying;
+export const getAudioDuration = (state) => state.ui.audioDuration;
+export const getAudioProgress = (state) => state.ui.audioProgress;
+export const getAudioRef = (state) => state.ui.audioRef;
+export const getProgressBarRef = (state) => state.ui.progressBarRef;
 export default uiSlice.reducer;
